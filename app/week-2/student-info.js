2a134d96034a15d17c048e160147b842c0df8bbd
ChesterLaraya <maximechester.laraya@edu.sait.ca>
2024-09-21 14:45:35 -0600

    week-2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+    return(
+        <header>
+            <h1>Chester Laraya</h1>
+
+             <a href= "https://github.com/ChesterLaraya?tab=repositories">Github Link </a>
+
+        </header>   
+    );
+}
